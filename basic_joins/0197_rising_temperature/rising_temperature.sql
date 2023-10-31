ded6b9ce1041b6d9afa1ee499be369f241af4de9
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2023-10-31 11:15:27 -0700

    Update rising_temperature.sql

--- basic_joins/0197_rising_temperature/rising_temperature.sql
+++ basic_joins/0197_rising_temperature/rising_temperature.sql
@@ -6,4 +6,4 @@ inner join
     weather as weather_yesterday
     on weather_today.recorddate = date_add(weather_yesterday.recorddate, interval 1 day)
 where
-  weather_today.temperature > weather_yesterday.temperature;
+    weather_today.temperature > weather_yesterday.temperature;
