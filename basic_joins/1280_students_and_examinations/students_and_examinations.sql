52da38e065fedab045f5fb2b0d70740139b623b6
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2023-10-31 11:50:09 -0700

    Create students_and_examinations.sql

---
+++ basic_joins/1280_students_and_examinations/students_and_examinations.sql
@@ -0,0 +1,20 @@
+select
+    s.student_id, 
+    s.student_name, 
+    sub.subject_name,
+    COUNT(e.subject_name) as attended_exams
+from
+    students as s
+cross join
+    subjects as sub
+left join
+    examinations as e
+    on  s.student_id = e.student_id and
+        sub.subject_name = e.subject_name
+group by
+    s.student_id,
+    s.student_name,
+    sub.subject_name
+order by
+    s.student_id,
+    sub.subject_name;
