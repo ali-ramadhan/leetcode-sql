b3d9e8e5a7ea81fec4ccec643c6d70532ffec08f
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2023-10-31 11:25:24 -0700

    Create average_time_of_process_per_machine.sql

---
+++ basic_joins/1661_average_time_of_process_per_machine/average_time_of_process_per_machine.sql
@@ -0,0 +1,14 @@
+select
+    start.machine_id,
+    round(avg(end.timestamp - start.timestamp), 3) as processing_time
+from
+    activity as start
+inner join
+    activity as end
+    on
+        start.machine_id = end.machine_id and
+        start.process_id = end.process_id and
+        start.activity_type = "start" and
+        end.activity_type = "end"
+group by
+    start.machine_id;
